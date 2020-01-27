0cc6f7f5680c6f4bebff60a3ec066a632c1513fb
neville bokdawalla <nevillekb@gmail.com>
2020-01-28 02:35:52 +0530

    - route handlers in the routes file - calling router regsiter func in main

--- api/routes.go
+++ api/routes.go
@@ -1,1 +1,21 @@
 package api
+
+import (
+	"email-validator/api/controller"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+// RegisterRoutes ...
+func RegisterRoutes() *chi.Mux {
+	router := chi.NewRouter()
+
+	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong..."))
+	})
+
+	router.Post("/email/validate", controller.Validate)
+
+	return router
+}
